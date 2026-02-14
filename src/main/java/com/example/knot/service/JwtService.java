f4d73c87af56b895106f99e2b98e4e18cc7e76ab
anubhavhajela4 <anubhavhajela4@gmail.com>
2026-02-14 23:54:06 +0530

    changed token expiry to 5 hours

--- src/main/java/com/example/knot/service/JwtService.java
+++ src/main/java/com/example/knot/service/JwtService.java
@@ -20,7 +20,7 @@ public class JwtService {
                 .setSubject(userId.toString())
                 .claim("role", role)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 5))
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
                 .compact();
     }
